f9f495d94a8bfe68d6acb1e82a47db0750208c1f
ProudMuslim <86370950+ProudMuslim@users.noreply.github.com>
2024-03-13 00:09:45 +0100

    Update guess_number.py

--- guess_number.py
+++ guess_number.py
@@ -1,6 +1,7 @@
 import sys
 import random
 
+
 def game_start():
     print(f"""I am thinking of a 3-digit number. Try to guess what it is.
 Here are some clues:
@@ -10,7 +11,8 @@ Here are some clues:
         Bagels              No digit is correct.
         """)
     while True:
-        launch_prompt = input("Would you like to begin or would you like to leave ( 'P' for play/ 'L' for leave ): ").lower()
+        launch_prompt = input(
+            "Would you like to begin or would you like to leave ( 'P' for play/ 'L' for leave ): ").lower()
         if launch_prompt == "l":
             sys.exit()
         elif launch_prompt == "p":
@@ -20,42 +22,40 @@ Here are some clues:
             continue
 
 
-def check_if_bagels(pc,player):
-    player_choice = []
-    for i in player:
-        player_choice.append(i)
-    if set(player_choice) & set(pc):
+def check_if_bagels(pc, player):
+    if set(player) & set(pc):
         return
     else:
         print("Bagels")
 
 
-
-def check_if_pico(pc,player):
-    pc_list = list(pc)
-    player_list = list(player)
-
-    for i in range(len(player_list)):
-        if player_list[i] in pc_list:
-            if player_list[i] != pc_list[i]:
+def check_if_pico_fermi(pc, player):
+    for i, k in enumerate(player):
+        if k in pc:
+            if player[i] != pc[i]:
                 print("pico")
-                
+            else:
+                print("fermi")
+    if pc in player:
+        print("You GUESSED IT !")
+        return True
+
 
 def main():
     pc_guess = random.randint(100, 999)
     str_pc_guess = str(pc_guess)
-    pc_guess_list = list(str_pc_guess)
+
     print(f"""I thought of a number!!!
 You have 10 guesses to find it """)
     i = 1
     print(pc_guess)
     for _ in range(10):
         player_guess = input(f"Guess #{i}: ")
-        check_if_bagels(pc_guess_list, player_guess)
-        check_if_pico(pc_guess_list, player_guess)
+        check_if_bagels(str_pc_guess, player_guess)
+        if check_if_pico_fermi(str_pc_guess, player_guess):
+            break
 
         i += 1
-
 
 
 game_start()
